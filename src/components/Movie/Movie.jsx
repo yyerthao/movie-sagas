60657e82ea2b92daa8c73e43e0098b09d0b54476
Yer Thao <maivyerthao@gmail.com>
2020-12-20 15:46:30 -0600

    Removed individual buttons for each input form, consolidated to submit button

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Movie.css';
 
+
 class Movie extends Component {
 
 
@@ -12,7 +13,7 @@ state = {
     title: '',
     poster: '',
     description: '',
-    genre: '1'
+    genre: ''
 }   
 
 // must utilizie componentDidMount to fetch all genres
@@ -22,10 +23,10 @@ componentDidMount(){
 }
 
 // function to bring user back to home
-goHome = () => {
-    console.log('Inside goHome button')
-    this.props.history.push('/');
-}
+// goHome = () => {
+//     console.log('Inside goHome button')
+//     this.props.history.push('/');
+// }
 
 //  handleChange function to handle all input changes 
 handleChange = (event, input) => {
@@ -37,6 +38,27 @@ handleChange = (event, input) => {
 }
 
 
+// brings user back to landing page, home
+cancelSubmit = () => {
+    this.props.history.push('/');
+// would we dispatch to a reducer to clear array? 
+}
+
+
+// function to collect all details from user and save to DB
+submitMovie = () => {
+    this.props.dispatch({type: 'POST_MOVIE', payload: this.state});
+    this.setState({
+        title: '',
+        poster: '',
+        description: '',
+        genre: ''
+    })
+    // after clicking on button 'Save', go back to main to see if movie was uploaded to list
+    this.props.history.push('/');
+}
+
+
     render() {
         return (
             <div>
@@ -44,21 +66,25 @@ handleChange = (event, input) => {
                 is being sent back from database */}
                 {JSON.stringify(this.props.reduxState.genres)};
                 {/* button to take user back home */}
-                <button onClick={this.goHome}>Go Home</button>
+                {/* <button onClick={this.goHome}>Go Home</button> */}
                 <br></br>
 
                 {/* applying handleTitle function to input form */}
-                <input onChange={this.handleChange}></input>
+                <input placeholder="Title" onChange={(event)=> this.handleChange(event, 'title')}></input>
                 {/* button to add title, will dispatch to post with saga then to reducer */}
-                <button onClick={this.addMovieDetails}>Add Title</button>
                 <br></br>
                 <br></br>
-                <input onChange={this.handleChange}></input>
-                <button onClick={this.addMovieDetails}>Add Poster Url</button>
+                <input placeholder="Poster URL" onChange={(event)=> this.handleChange(event, 'poster')}></input>
                 <br></br>
                 <br></br>
-                <input onChange={this.handleChange}></input>
-                <button onClick={this.addMovieDetails}>Add Description</button>
+                <textarea placeholder="Description" onChange={(event)=> this.handleChange(event, 'description')}></textarea>
+                <br></br>
+                <br></br>
+                <button onClick={this.cancelSubmit}>Cancel</button>
+                <button onClick={this.submitMovie}>Save and Submit</button>
+
+
+                <select value={this.state.genre} onChange={(event)=> this.handleChange(event, 'genre')}></select>
 
 
 
